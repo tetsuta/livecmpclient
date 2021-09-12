81314de088ff672c9589a103b1158169c1d39479
TAKAHASHI Tetsuro <tetsuvrx@gmail.com>
2021-09-12 12:28:40 +0900

    add server functions

--- lib/config.rb
+++ lib/config.rb
@@ -5,3 +5,12 @@ API_HASH = 'api_hash'
 
 TDLIB_PATH = '/usr/local/lib'
 
+LogFile = "../log/evaluation_server.log"
+LogLevel = :debug
+LogAge = 7
+LogSize = 10
+
+SystemPort = 8100
+SystemBindAddress = "0.0.0.0"
+HTTPAccessControl = "*"
+
--- lib/evaluation_server.rb
+++ lib/evaluation_server.rb
@@ -6,16 +6,16 @@ require 'net/protocol'
 require 'securerandom'
 require 'getoptlong'
 require 'logger'
+require 'net/protocol'
+require 'pp'
 
-require_relative './localconfig'
+require_relative './config'
 require_relative './client'
 
 # -------------------------------------------------- #
-opts = GetoptLong.new(
- 		      [ "--bot_name", "-b", GetoptLong::REQUIRED_ARGUMENT ],
- 		      [ "--number", "-n", GetoptLong::REQUIRED_ARGUMENT ],
-		      [ "--help", "-h", GetoptLong::NO_ARGUMENT ]
-		      )
+opts = GetoptLong.new( [ "--bot_name", "-b", GetoptLong::REQUIRED_ARGUMENT ],
+                       [ "--number", "-n", GetoptLong::REQUIRED_ARGUMENT ],
+                       [ "--help", "-h", GetoptLong::NO_ARGUMENT ] )
 
 def printHelp(message)
   STDERR.print message
@@ -51,17 +51,100 @@ if bot_name == nil || number_of_message == nil
 end
 
 # --------------------------------------------------
-telegram = Simple_TD.new(TDLIB_PATH, API_ID, API_HASH)
-telegram.select_chat(bot_name)
-
-if telegram.ready_to_talk?
-  message_list = telegram.get_recent_messages(number_of_message)
-  puts message_list.size
-  message_list.each{|m|
-    p m
-  }
+$logger = Logger.new(LogFile, LogAge, LogSize*1024*1024)
+case LogLevel
+when :fatal then
+  $logger.level = Logger::FATAL
+when :error then
+  $logger.level = Logger::ERROR
+when :warn then
+  $logger.level = Logger::WARN
+when :info then
+  $logger.level = Logger::INFO
+when :debug then
+  $logger.level = Logger::DEBUG
 end
 
-puts "Closing..."
-telegram.close()
+
+
+options = {
+  :Port => SystemPort,
+  :BindAddress => SystemBindAddress,
+  :DoNotReverseLookup => true
+}
+
+s = WEBrick::HTTPServer.new(options)
+
+
+s.mount_proc('/'){|request, response|
+  errormsg = "request body error."
+  begin
+    data = Hash::new
+    data["text"] = ""
+
+    if (request.request_method != "POST")
+      errormsg = "HTTP method error."
+      raise ArgumentError.new(errormsg)
+    end
+    if (request.content_type == nil)
+      errormsg = "content-type error."
+      raise ArgumentError.new(errormsg)
+    end
+    if (request.body == nil)
+      errormsg = "request body error. bodysize=nil"
+      raise ArgumentError.new(errormsg)
+    end
+
+    userInput = JSON.parse(request.body)
+    input = userInput["input"]
+
+    $logger.info("connection: __#{sessionID}__:#{request.peeraddr.to_s}")
+    data["text"] << Time.now.to_s
+    response.body = JSON.generate(data)
+
+  rescue Exception => e
+    $logger.fatal(e.message)
+    $logger.fatal(e.class)
+    $logger.fatal e.backtrace
+    errdata = Hash::new
+    errbody = Hash::new
+    case e
+    when Net::ReadTimeout then
+      response.status = 408
+    when Net::ProtoAuthError then
+      response.status = 401
+    else
+      response.status = 500
+    end
+    errbody["code"] = response.status
+    errbody["message"] = e.message
+    errdata["error"] = errbody
+    response.body = JSON.generate(errdata)
+  ensure
+    if (HTTPAccessControl != nil && HTTPAccessControl != "")
+      response.header["Access-Control-Allow-Origin"] = HTTPAccessControl
+    end
+    response.content_type = "application/json; charset=UTF-8"
+  end
+}
+
+Signal.trap(:INT){
+  s.shutdown
+}
+
+s.start
+
+
+
+# telegram = Simple_TD.new(TDLIB_PATH, API_ID, API_HASH)
+# telegram.select_chat(bot_name)
+# if telegram.ready_to_talk?
+#   message_list = telegram.get_recent_messages(number_of_message)
+#   puts message_list.size
+#   message_list.each{|m|
+#     p m
+#   }
+# end
+# puts "Closing..."
+# telegram.close()
 
