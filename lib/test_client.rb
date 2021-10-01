1cbcc1582189cd15a2a6c999d855e391853445b0
TAKAHASHI Tetsuro <tetsuvrx@gmail.com>
2021-10-01 16:28:38 +0900

    add quiet mode

--- lib/test_client.rb
+++ lib/test_client.rb
@@ -8,6 +8,7 @@ require './client'
 # -------------------------------------------------- #
 opts = GetoptLong.new(
  		      [ "--bot_name", "-b", GetoptLong::REQUIRED_ARGUMENT ],
+		      [ "--quiet", "-q", GetoptLong::NO_ARGUMENT ],
 		      [ "--help", "-h", GetoptLong::NO_ARGUMENT ]
 		      )
 
@@ -18,17 +19,21 @@ end
 
 USAGE_MESSAGE = "
 Usage:
- ./test_client.rb [-h] -b bot_name_bot
+ ./test_client.rb [-h] [-q] -b bot_name_bot
 
 -h: show help messsage
+-q: quiet mode
 -b: bot_name
 "
 
 bot_name = nil
+quiet = false
 opts.each{|opt, arg|
   case opt
   when "--help"
     printHelp(USAGE_MESSAGE)
+  when "--quiet"
+    quiet = true
   when "--bot_name"
     bot_name = arg
   end
@@ -39,7 +44,7 @@ if bot_name == nil
 end
 
 # --------------------------------------------------
-telegram = Simple_TD.new(TDLIB_PATH, API_ID, API_HASH)
+telegram = Simple_TD.new(TDLIB_PATH, API_ID, API_HASH, quiet)
 telegram.select_chat(bot_name)
 
 if telegram.ready_to_talk?
